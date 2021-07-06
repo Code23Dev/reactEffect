42b19f70dccfca361940ac09b85e4c9c37fe76ae
leman ibrahimli <65496038+Code23Dev@users.noreply.github.com>
2021-07-06 12:41:52 +0400

    use effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [message,setMessage] = useState("inirial") //create state
+  const [messageData,setMessageData] = useState("inirial data") //create state
+  useEffect(function (){console.log("effect")},[message,messageData]) //create effect
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Effect</h1>
+      {message}
+      <button onClick={() => {
+         setMessage("hi!")
+       }}>
+        change text
+      </button>
+      <br/>
+      {messageData}
+      <button onClick={() => {
+        setMessageData("hi data!")
+      }}>
+        change text
+      </button>
     </div>
   );
 }
 
 export default App;
+
+// state and effect import in react folder
